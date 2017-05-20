69a1052bf0977f30eeb94c8ac551c31815aa3d4f
Chris Heald <cheald@gmail.com>
2017-05-19 19:12:10 -0700

    Add a lookup map for handlers by domain

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 "use strict";
 const config = require('./config');
 const IRC = require('irc');
-const fs = require('fs');
 const request = require('request');
-const commands = {channel: new Map(), pm: new Map(), raw: new Map()}
 const cheerio = require("cheerio");
 const URL = require('url');
 
@@ -11,23 +9,28 @@ var bot;
 
 const URL_RE = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i
 
+const domainHandlers = new Map();
+
+domainHandlers.set("twitter.com", ($) => {
+  const handle = ($("meta[property='og:url']").attr("content") || "").split("/")[3] || "unknown";
+  const tweet = $("meta[property='og:description']").attr("content").replace(/\n/, " ").replace(/^“|”$/g, "");
+  const author = ($("meta[property='og:title']").attr("content") || "").replace(" on Twitter", "");
+  const tweetContainer = $("div.tweet.permalink-tweet");
+  let date = "";
+  let verified = "";
+  if (tweetContainer.length > 0) {
+    date = tweetContainer.find("a.tweet-timestamp span.u-hiddenVisually").text();
+    verified = tweetContainer.find("span.Icon--verified").length > 0 ? " ✔" : "";
+  }
+  return `TWEET: ${tweet} -- ${author} (@${handle}${verified}) ${date}`;
+});
+domainHandlers.set("www.twitter.com", domainHandlers.get("twitter.com"));
+
 function handleBody(from, url, response) {
   const $ = cheerio.load(response.body);
-  let str = "";
-  if (url.host.match("twitter.com")) {
-    const handle = ($("meta[property='og:url']").attr("content") || "").split("/")[3] || "unknown";
-    const tweet = $("meta[property='og:description']").attr("content").replace(/\n/, " ").replace(/^“|”$/g, "");
-    const author = ($("meta[property='og:title']").attr("content") || "").replace(" on Twitter", "");
-    const tweetContainer = $("div.tweet.permalink-tweet");
-    let date = "";
-    let verified = "";
-    if (tweetContainer.length > 0) {
-      date = tweetContainer.find("a.tweet-timestamp span.u-hiddenVisually").text();
-      verified = tweetContainer.find("span.Icon--verified").length > 0 ? " ✔" : "";
-    }
-    str = `TWEET: ${tweet} -- ${author} (@${handle}${verified}) ${date}`;
-  } else {
-    str = $("head title").text();
+  let str = $("head title").text();
+  if (domainHandlers.has(url.host)) {
+    str = domainHandlers.get(url.host)($);
   }
   if (str && str != "") {
     bot.say(from, str);

068db0616ab6954b20e8d8c951fac7aa383183de
Chris Heald <cheald@gmail.com>
2017-05-19 20:44:12 -0700

    Support for indicating the presence of image/video on tweets

--- index.js
+++ index.js
@@ -13,14 +13,25 @@ const domainHandlers = new Map();
 
 domainHandlers.set("twitter.com", ($) => {
   const handle = ($("meta[property='og:url']").attr("content") || "").split("/")[3] || "unknown";
-  const tweet = $("meta[property='og:description']").attr("content").replace(/\n/, " ").replace(/^“|”$/g, "");
   const author = ($("meta[property='og:title']").attr("content") || "").replace(" on Twitter", "");
   const tweetContainer = $("div.tweet.permalink-tweet");
+  let tweet = $("meta[property='og:description']").attr("content").replace(/\n/, " ").replace(/^“|”$/g, "");
   let date = "";
   let verified = "";
+  let image = "";
+  let images = $("meta[property='og:image:user_generated']").length
+  let videos = $("meta[property='og:video:url']").length
   if (tweetContainer.length > 0) {
     date = tweetContainer.find("a.tweet-timestamp span.u-hiddenVisually").text();
     verified = tweetContainer.find("span.Icon--verified").length > 0 ? " ✔" : "";
+    const image = tweetContainer.find("div.AdaptiveMedia-photoContainer.js-adaptive-photo ").attr("data-image-url");
+    if (videos > 0) {
+      let video = $("meta[property='og:video:url']").attr("content");
+      tweet += ` (Video: ${video})`
+    } else if (images > 0) {
+      let image = $("meta[property='og:image']").attr("content");
+      tweet += ` (Image (1/${images}): ${image})`
+    }
   }
   return `TWEET: ${tweet} -- ${author} (@${handle}${verified}) ${date}`;
 });

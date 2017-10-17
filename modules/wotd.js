9982ee0ce71052bca5d7a9282346c8b0fd5676fa
Robert Grimes <rmzgrimes@gmail.com>
2017-10-17 11:18:02 -0500

    Added new wotd module
    `wotd` command pulls from https://www.merriam-webster.com/word-of-the-day
    requires: cheerio, request
    
    Also fixed the module loader to skip non js files in modules/

---
+++ modules/wotd.js
@@ -0,0 +1,36 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+let _bot = null;
+
+function wotd(bot, words, from, to) {
+	request("https://www.merriam-webster.com/word-of-the-day", function( error, response, body ) {
+		const sendTo = to == _bot.nick ? from : to;
+
+		if( !body ) { bot.say( sendTo, "wotd error: " + error ); return }
+
+		let $ = cheerio.load(body);
+		let word = $('.word-and-pronunciation H1').text().trim();
+
+		let definitions = [];
+		let definition_start = $('.wod-definition-container H2');
+		while( definition_start = definition_start.next() ) {
+			if( definition_start.get(0).tagName != 'p' ) break;
+
+			definitions.push( definition_start.eq(0).text().trim() );
+		}
+
+		let definition_string = definitions.join("  ");
+
+		bot.say( sendTo, `${word}: ${definition_string}` );
+	});
+}
+
+function setup(bot, commands) {
+  _bot = bot;
+  commands.set("wotd", wotd);
+}
+
+module.exports = {
+  setup: setup
+};

d35420c5ee0b48e24ecb22a5495edb2f8d10d432
Chris Heald <cheald@gmail.com>
2017-11-08 12:36:06 -0700

    Update WOTD module

--- modules/wotd.js
+++ modules/wotd.js
@@ -1,29 +1,45 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const winston = require("winston");
 
 let _bot = null;
 
-function wotd(bot, words, from, to) {
-	request("https://www.merriam-webster.com/word-of-the-day", function( error, response, body ) {
-		const sendTo = to == _bot.nick ? from : to;
+function wotd(bot, _words, from, to) {
+  request("https://www.merriam-webster.com/word-of-the-day", function(
+    error,
+    response,
+    body
+  ) {
+    const sendTo = to == _bot.nick ? from : to;
 
-		if( !body ) { bot.say( sendTo, "wotd error: " + error ); return }
+    if (!body) {
+      bot.say(sendTo, "wotd error: " + error);
+      return;
+    }
 
-		let $ = cheerio.load(body);
-		let word = $('.word-and-pronunciation H1').text().trim();
+    let $ = cheerio.load(body);
+    let word = $(".word-and-pronunciation H1")
+      .text()
+      .trim();
 
-		let definitions = [];
-		let definition_start = $('.wod-definition-container H2');
-		while( definition_start = definition_start.next() ) {
-			if( definition_start.get(0).tagName != 'p' ) break;
+    let definitions = [];
+    let definition_start = $(".wod-definition-container H2");
+    while ((definition_start = definition_start.next())) {
+      if (definition_start.get(0).tagName != "p") break;
 
-			definitions.push( definition_start.eq(0).text().trim() );
-		}
+      definitions.push(
+        definition_start
+          .eq(0)
+          .text()
+          .trim()
+      );
+    }
 
-		let definition_string = definitions.join("  ");
+    let definition_string = definitions.join("  ");
+    winston.info(`"Sending wotd to ${sendTo}`);
 
-		bot.say( sendTo, `${word}: ${definition_string}` );
-	});
+    bot.say(sendTo, `${word}: ${definition_string}`);
+  });
 }
 
 function setup(bot, commands) {

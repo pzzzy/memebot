fda836a27a2baefe898be9091a0993d1b27baabe
Robert Grimes <rmzgrimes@gmail.com>
2018-01-18 16:05:31 -0600

    Updated output format for stock quotes.

--- modules/quote.js
+++ modules/quote.js
@@ -1,4 +1,5 @@
 const YahooStocks = require('yahoo-stocks');
+const util = require('util');
 
 let _bot = null;
 
@@ -6,7 +7,16 @@ function quote(bot, _words, from, to) {
 	const sendTo = to == _bot.nick ? from : to;
 
 	YahooStocks.lookup(_words[1]).then(function(res){
-		bot.say(sendTo, `${res.name}: \$${res.currentPrice}`);
+		let curr = res;
+		YahooStocks.history(_words[1]).then(function(res){
+			let old = res.previousClose;
+			let change =  curr.currentPrice - old;
+			let percentage = ( change / curr.currentPrice ) * 100;
+				percentage = Math.round(percentage * 100) / 100;
+				change = Math.round(change*100)/100;
+
+			bot.say(sendTo, `${curr.name}: \$${curr.currentPrice}; Change since yesterday: \$${change} (${percentage}%)`);
+		});
 	}).catch(function(err) {
 		bot.say(sendTo, "quote error " + err);
 	});

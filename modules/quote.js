4de9516b4550da0d7564161a30c05c2f163203c8
Chris Heald <cheald@gmail.com>
2019-05-22 10:33:05 -0700

    Use change since open

--- modules/quote.js
+++ modules/quote.js
@@ -1,29 +1,33 @@
-const YahooStocks = require('yahoo-stocks');
-const util = require('util');
+const YahooStocks = require("yahoo-stocks");
+const util = require("util");
 
 let _bot = null;
 
 function quote(bot, _words, from, to) {
-	const sendTo = to == _bot.nick ? from : to;
+  const sendTo = to == _bot.nick ? from : to;
 
-	YahooStocks.lookup(_words[1]).then(function(res){
-		let curr = res;
-		YahooStocks.history(_words[1]).then(function(res){
-			let old = res.previousClose;
-			let change =  curr.currentPrice - old;
-			let percentage = ( change / curr.currentPrice ) * 100;
-				percentage = Math.round(percentage * 100) / 100;
-				change = Math.round(change*100)/100;
+  YahooStocks.lookup(_words[1])
+    .then(function(res) {
+      let curr = res;
+      YahooStocks.history(_words[1]).then(function(res) {
+        let old = res.records[res.records.length - 1].open;
+        let change = curr.currentPrice - old;
+        let percentage = (change / curr.currentPrice) * 100;
+        percentage = Math.round(percentage * 100) / 100;
+        change = Math.round(change * 100) / 100;
 
-			bot.say(sendTo, `${curr.name}: \$${curr.currentPrice}; Change since yesterday: \$${change} (${percentage}%)`);
-		});
-	}).catch(function(err) {
-		bot.say(sendTo, "quote error " + err);
-	});
-
+        bot.say(
+          sendTo,
+          `${curr.name}: \$${
+            curr.currentPrice
+          }; Change since open: \$${change} (${percentage}%)`
+        );
+      });
+    })
+    .catch(function(err) {
+      bot.say(sendTo, "quote error " + err);
+    });
 }
-
-
 
 function setup(bot, commands) {
   _bot = bot;
@@ -33,4 +37,3 @@ function setup(bot, commands) {
 module.exports = {
   setup: setup
 };
-
